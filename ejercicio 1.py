639d8e329a0e1836cf6b1dfb571f357620a68965
Alejandro Aponte <yasmiverromero@gmail.com>
2024-06-17 05:49:47 -0500

    Hice varias operaciones y use el método type

---
+++ ejercicio 1.py
@@ -0,0 +1,26 @@
+#Operadores matematicos
+print(3 + 4)
+print(3 - 4)
+print(3 / 4)
+print(3 * 4)
+print(3**4)
+print(3 % 4)
+print(3// 4)
+
+
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+print("Hola soy Alejo soy muy joven, vivo en Colombia")
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4.4j))
+print(type(["hola","python", "Mexico"]))
+
