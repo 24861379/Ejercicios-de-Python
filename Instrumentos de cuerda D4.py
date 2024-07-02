5c8a63acc08e04455c2577262fe16a509aa836ba
Alejandro Aponte <yasmiverromero@gmail.com>
2024-07-02 14:16:45 -0500

    Empece con el dia 4 de python con un tema de cadenas de texto

---
+++ Instrumentos de cuerda D4.py
@@ -0,0 +1,22 @@
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+space = ' '
+full_name = first_name  +  space + last_name
+print(full_name) # Asabeneh Yetayeh
+
+#FORMATO DE CADENA
+nombre= 'Carlos'
+Apellido = 'Rodrigues'
+lenguaje = 'python'
+'''formato_de_cadena= 'soy %s %s. y estoy trabajando con %s' %(nombre, Apellido, lenguaje)
+print(formato_de_cadena)'''
+#--------------------------------------------------------------------------------------
+#str.format
+formato_de_cadena = 'soy {}{}.. y estoy trabajando con {}'.format(nombre, Apellido, lenguaje)
+#--------------------------------------------------------------------------------------
+#STRINGS Y NUMEROS
+radio = 10
+pi = 3.14
+area = pi * radio **2
+formato_de_CADENA='El area del circulo es de %d es %f' (radio, area)
+
