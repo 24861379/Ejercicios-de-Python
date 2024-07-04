1b4a5b3ebcb2a027c1a02cee71b38ea177f7d424
Alejandro Aponte <yasmiverromero@gmail.com>
2024-07-04 14:15:42 -0500

    termine el dia 4 de python :)

--- Instrumentos de cuerda D4.py
+++ Instrumentos de cuerda D4.py
@@ -18,5 +18,30 @@ formato_de_cadena = 'soy {}{}.. y estoy trabajando con {}'.format(nombre, Apelli
 radio = 10
 pi = 3.14
 area = pi * radio **2
-formato_de_CADENA='El area del circulo es de %d es %f' (radio, area)
+formato_de_CADENA='El area del circulo es de %d es %.2f' %(radio, area)
 
+#------------------------------------------------------------------------------------------
+#MÉTODOS DE CADENAS
+challenge = 'treinta días de python'
+print(challenge.capitalize()) # --> convierte el primer caracter de la cadena en letra mayúscula
+
+#endswith() --> comprueba si la cadena termina en una palabra especifica
+print(challenge.endswith('as')) #false
+print(challenge.endswith('on')) #true
+
+#expandtabs() --> reemplaza el caracter de espacio por tabulación el cual tiene 8 espacios
+Challenge = 'treinta\tdías\tde\tpython'
+print(Challenge.expandtabs()) #treinta días    de      python
+print(Challenge.expandtabs(10)) #treinta   días      de        python
+
+#isalnumber() --> Comprueba caracteres alfanuméricos
+reto ='treintadíasdepython'
+print(reto.isalnum())#true
+Reto = '30díasdepython'
+print(reto.isalnum()) #true
+REto='treinta días de python'
+print(reto.isalnum()) #true
+RETo='treinta días de python 2024'
+print(reto.isalnum()) #true
+#replace() --> reemplaza la subcadena con una cadena dada
+print(challenge.replace('python', 'programación'))
---
+++ ejercicio D4/ejercicio 1.py
@@ -0,0 +1,9 @@
+empresa = 'codificación para todos'
+print('Altero la cadena de texto con el método upper(): ', empresa.upper())
+print('Altero la cadena de texto con el método lower(): ',empresa.lower())
+print('Altero la cadena de texto con el método capitalize(): ', empresa.capitalize())
+print('Altero la cadena de texto con el método title(): ', empresa.title())
+print('Altero la cadena de texto con el método swapcase(): ' ,empresa.swapcase())
+print('con el metodo find() averiguo si esta la para codificacion: ', empresa.find('codificación'))
+print('Reemplazo la \"codificación para todos\" por python: ', empresa.replace('codificación para todos', 'python'))
+print('Divido la cadena con el método \"split()\": ', empresa.split())
